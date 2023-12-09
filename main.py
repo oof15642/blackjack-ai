d32a314f8dcf1e84c160ac9631f1c7ab0b84d2c3
oof15642 <59772241+oof15642@users.noreply.github.com>
2023-12-09 00:14:52 -0700

    uhh some issues check description
    ValueError: invalid literal for int() with base 10: 'ace',
    and won't draw properly.

--- main.py
+++ main.py
@@ -1,110 +1,84 @@
 # Imports
 import random
+from time import sleep
 
 # Variables
 suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
-ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "ace"]
+ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "10", "10", "10", "ace"]
 
 deck = [[rank, suit] for suit in suits for rank in ranks]
 
 # Functions
 def hand_value(hand):
-  value = 0
-  for card in hand:
-    if card[0] != "ace":
-      value += int(card[0])
-    else:
-      if value + 11 > 21:
-        value += 1
-      else:
-        value += 11
+	value = 0
+	for card in hand:
+		if card[0] != "ace":
+			value += int(card[0])
+		else:
+			if value + 11 > 21:
+				value += 1
+			else:
+				value += 11
 
-  return value
+	return value
+
 
 # Classes
-class ais:
-    
-    def __init__(self):
-        self.self = self
-        self.handOne = []
-        self.handTwo = []
-        self.valueOne = 0
-        self.valueTwo = 0
-        
-    def draw_card(self, handDrawTo):
-        global deck
-        
-        cardDraw = random.choice(deck)
-        handDrawTo.append(deck[deck.index(cardDraw)])
-        deck.pop(deck.index(cardDraw))
-        
-        if handDrawTo[0] == "ace" and self.handDrawTo:
-            
-        
-    def populate_hands(self):
-        draw_card(self.handOne)
-        draw_card(self.handOne)
-        
-        draw_card(self.handTwo)
-        draw_card(self.handTwo)
-        
-        
-        
-    def player_move_one(self):
-        
-    
-    
-  def make_move(self, handValue):
-    if handValue < 17:
-      print("Dealer hits")
-      cardDrawThree = random.choice(deck)
-      self.hand.append(deck[deck.index(cardDrawThree)])
-      deck.pop(deck.index(cardDrawThree))
 
-class aiTwoClass:
-  def __init__(self):
-    self.self = self
-    self.hand = []
+### Ai Class ###
+class Ai:
+	
+	def __init__(self):
+		self.self = self
+		self.hand = []
+		self.value = 0
+		
+	def draw_card(self):
+		global deck
+		
+		cardDraw = random.choice(deck)
+		self.hand.append(deck[deck.index(cardDraw)])
+		deck.pop(deck.index(cardDraw))
 
-  def draw(self):
-    cardDrawOne = random.choice(deck)
-    self.hand.append(deck[deck.index(cardDrawOne)])
-    deck.pop(deck.index(cardDrawOne))
+		for card in self.hand:
+			if str(card[0]) != "ace":
+				self.value += int(cardDraw[0])
 
-    cardDrawTwo = random.choice(deck)
-    self.hand.append(deck[deck.index(cardDrawTwo)])
-    deck.pop(deck.index(cardDrawOne))
+			elif self.value + 11 > 21:
+				self.value += 1
 
-  def make_move_one(self, handValue, aiOneHand):
-    
+			else:
+				self.value += 11
 
-    
+	def dealer_moves(self):
+		while self.value <= 17:
+			self.draw_card()
+			return self.value
+		
+	def populate_hand(self):
+		self.draw_card()
+		self.draw_card()
 
+### Game Class ###
 class Game():
 
-  def __init__(self, aiOne, aiTwo):
-    self.self = self
-    self.aiOne = aiOne
-    self.aiTwo = aiTwo
+	def __init__(self):
+		self.self = self
 
-  def play(self):
-    self.aiOne.draw()
-    self.aiTwo.draw()
+	def play(self):
+		aiDealer = Ai()
+		aiPlayer = Ai()
 
-    self.aiOneValue = hand_value(self.aiOne.hand)
-    self.aiTwoValue = hand_value(self.aiTwo.hand)
+		aiDealer.populate_hand()
+		aiPlayer.populate_hand()
 
-    if self.aiOneValue != 21:
-      print("The dealer has a hand of " + str(self.aiOneValue))
-      print("You have a hand of " + str(self.aiTwoValue))
-
-      if self.aiOneValue > self.aiTwoValue:
-      
-
-    else:
-      print("AI One Wins!")
-      exit()
-      
-
-    
+		
+		while aiDealer.value <= 21:
+			aiDealer.dealer_moves()
+			print(aiDealer.hand)
+			sleep(1)
+	
 # Game run
+initGame = Game()
+
+initGame.play()
